c0166640f252d21e7ce6499b637adae5ce2a521b
xCrazyxx <xcrazycapx@gmail.com>
2020-09-13 21:28:56 -0700

    Navigation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer'
+import { env, selectors } from './env'
+;(async () => {
+	const browser = await puppeteer.launch({ headless: false })
+	const page = await browser.newPage()
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
+	await page.goto(env.url)
+	await page.waitFor(selectors.email)
 
-  const page = await browser.newPage();
+	await page.type(selectors.email, env.username)
+	await page.type(selectors.password, env.password)
 
-  await page.goto('https://google.com');
+	await page.click(selectors.submit)
+	await page.waitForNavigation({ waitUntil: 'networkidle0' })
 
-  browser.close();
-})();
+	await page.goto(env.userurl)
+})()
